ec6152d4904d8faa41a27e0f3ad61a2b8d7f40a1
Nacer-78 <nacer.bouamama.78@gmail.com@gmail.com>
2023-06-08 11:26:22 +0200

    Modif du header

--- view/header.php
+++ view/header.php
@@ -9,14 +9,20 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" href="#">Présentation</a>
+                <a class="nav-link" href="#">Notre formation</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Espace Etudiant</a>
+                <a class="nav-link" href="#">Se connecter</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Espace Entreprise</a>
-              </li>
+              <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="menuConnexionDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Se connecter
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="menuConnexionDropdown">
+                            <li><a class="dropdown-item" href="#">Elèves / Enseignant</a></li>
+                            <li><a class="dropdown-item" href="#">Entreprise</a></li>
+                        </ul>
+                    </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Personnel</a>
               </li>
@@ -33,4 +39,4 @@
             <img src="images/login.svg" alt="Bootstrap" width="70" height="75">
           </a>
         </div>
-      </nav>
+</nav>
