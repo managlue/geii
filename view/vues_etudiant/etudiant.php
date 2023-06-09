a88b106e34afa392e50f2872ac4719e00cb27874
managlue <125822488+managlue@users.noreply.github.com>
2023-06-09 19:15:47 +0200

    Update etudiant.php

--- view/vues_etudiant/etudiant.php
+++ view/vues_etudiant/etudiant.php
@@ -3,22 +3,24 @@
 <html lang="FR">
 
     <?php
-        
         include '../../modele/connexionBd.php';
         session_start();
     ?>
 
     <head>
-
         <meta charset="utf-8">
 
         <!-- https://getbootstrap.com/docs/5.3/getting-started/introduction/ -->
-       
         <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <script src="../../bootstrap/js/bootstrap.min.js"></script>
 
         <!-- Autres CSS -->
         <link href="../../css/Etudiant_CSS/edt.css" rel="stylesheet">
+        <link href="../../css/Etudiant_CSS/menu.css" rel="stylesheet">
+
+        <!-- Autres JS -->
+        <script src="../../script/etudiant/boutonMenu.js"></script>
+
 
     </head>
 
@@ -26,24 +28,38 @@
 
     <?php include '../header.php'; ?>
 
-    <!-- début du contenu de la page -->
+    <!-- Page content holder -->
+    <div class="page-content p-5" id="content">
 
-    <?php
-        echo 'Bienvenue ' . $_SESSION['nom'] . ' ' . $_SESSION['prenom'] . ' !';
+        <!-- Toggle button -->
+        <button id="sidebarCollapse" type="button" class="btn btn-light bg-white rounded-pill shadow-sm px-4 mb-4">
+            <i class="fa fa-bars mr-2"></i>
+            <small class="text-uppercase font-weight-bold">Toggle</small>
+        </button>
 
-        // voir les dernières notes
-        $limit = 1;
-        include '../../modele/etudiant/getMarks.php';
+        <!-- content -->
 
-        // voir l'emplois du temps
-        $today = true;
-        include '../../modele/etudiant/getEdt.php';
+        <?php include 'menu.php'; ?>
 
-        // voir les supports de cours
-        // include '../modele/etudiant/.php';
-    ?>
+        <!-- début du contenu de la page -->
 
-    <!-- fin du contenu de la page -->
+        <?php
+            echo 'Bienvenue ' . $_SESSION['nom'] . ' ' . $_SESSION['prenom'] . ' !';
+
+            // voir les dernières notes
+            $limit = 1;
+            include '../../modele/etudiant/getMarks.php';
+
+            // voir l'emplois du temps
+            $today = true;
+            include '../../modele/etudiant/getEdt.php';
+
+            // voir les supports de cours
+            // include '../modele/etudiant/.php';
+        ?>
+
+        <!-- fin du contenu de la page -->
+    </div>
 
     <?php include '../footer.php'; ?>
 
