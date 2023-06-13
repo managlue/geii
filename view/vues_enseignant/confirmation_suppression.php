8bddcc38235b0c32d4018e3499b705a83aa77139
Marco Chm <91530804+MarcoCihm@users.noreply.github.com>
2023-06-13 14:37:52 +0200

    Fonctionnel

--- view/vues_enseignant/confirmation_suppression.php
+++ view/vues_enseignant/confirmation_suppression.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Confirmation de la suppression</title>
 </head>
+
 <body>
     <h1>Confirmation de la suppression</h1>
     <p>La note a été supprimée avec succès.</p>
     <a href="../vues_enseignant/enseignant.php">Retour à la page d'accueil</a>
 </body>
+
 </html>
