d255f3fec60cb465fe8fd2019d44822b6a7ce998
Marco Chm <91530804+MarcoCihm@users.noreply.github.com>
2023-06-14 01:50:16 +0200

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,72 +1,73 @@
 <head>
 
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- CSS -->
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-<link href="../bootstrap-5.3/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
-<link rel="stylesheet" href="../css/Footer/style.css">
+    <!-- CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="../bootstrap-5.3/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
+    <link rel="stylesheet" href="../css/Footer/style.css">
 
 </head>
 
 <footer>
-<div class="footer-top">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 footer-about">
-                <img class="logo-footer" src="../../assets/icons/GEII.png" alt="logo-footer"
-                    data-at2x="../../assets/icons/GEII.png">
-                <p>
-      Bienvenue à l'IUT d'Évry Val d'Essonne ! 
-                </p>
-            </div>
-            <div class="col-md-4 offset-md-1 footer-contact">
-                <h3>Contact</h3>
-                <p><i class="fas fa-map-marker-alt"></i> 25 cours Monseigneur Roméro,91000 Évry</p>
-                <p><i class="fas fa-phone"></i> 01 69 47 72 20</p>
-                <p><i class="fas fa-envelope"></i> Email: <a href="mailto:secretariat-geii@iut.univ-evry.fr">secretariat-geii@iut.univ-evry.fr</a>
-                </p>
-            </div>
-            <div class="col-md-4 footer-links ">
-                <!-- <div class="row">
+    <div class="custom-footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 footer-about">
+                        <img class="logo-footer" src="../../assets/icons/GEII.png" alt="logo-footer"
+                            data-at2x="../../assets/icons/GEII.png">
+                        <p>
+                            IUT Évry Val d'Essonne
+                        </p>
+                    </div>
+                    <div class="col-md-4 offset-md-1 footer-contact">
+                        <h3>Contact</h3>
+                        <p><i class="fas fa-map-marker-alt"></i> 25 cours Monseigneur Roméro, 91000 Évry</p>
+                        <p><i class="fas fa-phone"></i> 01 69 47 72 20</p>
+                        <p><i class="fas fa-envelope"></i> Email: <a
+                                href="mailto:secretariat-geii@iut.univ-evry.fr">secretariat-geii@iut.univ-evry.fr</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 footer-links ">
+                        <!-- <div class="row">
                     <div class="col">
                         <h3>Lien</h3>
                     </div>
                 </div> -->
-                <div class="row">
-                    <div class="col-md-6">
-                        <p><a class="scroll-link" href="#top-content">Accueil</a></p>
-                        <p><a href="#">Espace Entreprise</a></p>
-                        <p><a href="#">Espace Enseignant</a></p>
-                        <p><a href="#">Espace Etudiants</a></p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p><a class="scroll-link" href="../vues_formation/formations.php">Nos Formations</a></p>
+                                <p><a href="../vues_formation/programme.php">Les Programmes</a></p>
+                                <p><a href="../vues_formation/alternance.php">Alternance</a></p>
+                            </div>
+                            <div class="col-md-6">
+                                <p><a href="../vues_formation/futur.php">Après le DUT GEII </a></p>
+                                <p><a href="../vues_formation/mentions.php">Mentions légales</a></p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <p><a href="#">Plan du site</a></p>
-                        <p><a href="#">Mentions légales</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 footer-copyright">
+                        &copy; 2023 IUT d’Évry Val d’Essonne
+                    </div>
+                    <div class="col-md-6 footer-social">
+                        <a href="https://www.facebook.com/iutevry"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/Alumnieve"><i class="fab fa-twitter"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="footer-bottom">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 footer-copyright">
-                &copy; 2023 IUT d’Évry Val d’Essonne 
-            </div>
-            <div class="col-md-6 footer-social">
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-        </div>
-    </div>
-</div>
 </footer>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="../bootstrap-5.3/js/bootstrap.bundle.js"></script>
 </script>
