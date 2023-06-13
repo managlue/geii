0ecf4254b741e8b282076f25e8d92568d4ffc885
Marco Chm <91530804+MarcoCihm@users.noreply.github.com>
2023-06-13 14:37:08 +0200

    Fonctionnel

--- view/vues_enseignant/confirmation.php
+++ view/vues_enseignant/confirmation.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Confirmation</title>
 </head>
+
 <body>
     <h1>Confirmation</h1>
     <p>Les notes ont été ajoutées avec succès.</p>
     <a href="../vues_enseignant/enseignant.php">Retour à la page d'accueil</a>
 </body>
+
 </html>
