83b6b3721b7ac12099e2168927455819890e57e0
Marco Chm <91530804+MarcoCihm@users.noreply.github.com>
2023-06-14 01:51:47 +0200

    Add files via upload

---
+++ view/vues_formation/programme.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="FR">
+
+<head>
+    <meta charset="utf-8">
+    <title>Site web GEII</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap" rel="stylesheet">
+    <link href="../../bootstrap-5.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../../bootstrap-5.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../css/Footer/style.css">
+
+<?php  
+    include '../footer.php';
+?>
