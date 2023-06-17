7c4601f47419175cdd5aac5e140bb3d2ebceed22
managlue <125822488+managlue@users.noreply.github.com>
2023-06-17 19:19:52 +0200

    Aa

--- view/vues_etudiant/edt.php
+++ view/vues_etudiant/edt.php
@@ -10,21 +10,21 @@
     <head>
         <meta charset="utf-8">
 
-        <!-- https://getbootstrap.com/docs/5.3/getting-started/introduction/ -->
-        <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <!-- https://getbootstrap.com/docs/5.3/getting-started/introduction/ -->
+    <link href="/geii/bootstrap-5.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/geii/bootstrap-5.3/js/bootstrap.min.js"></script>
 
-        <!-- Autres JS -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="../../script/etudiant/boutonMenu.js"></script>
-        <script src="../../script/etudiant/faviconSwap.js"></script>
+    <!-- Autres JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/geii/script/etudiant/boutonMenu.js"></script>
+    <script src="/geii/script/etudiant/faviconSwap.js"></script>
 
-        <!-- https://fontawesome.com/v4/icons/ -->
-        <link href="../../font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- https://fontawesome.com/v4/icons/ -->
+    <link href="/geii/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-        <!-- Autres CSS -->
-        <link href="../../css/Etudiant_CSS/edt.css" rel="stylesheet">
-        <link href="../../css/Etudiant_CSS/menu.css" rel="stylesheet">
+    <!-- Autres CSS -->
+    <link href="/geii/css/Etudiant_CSS/edt.css" rel="stylesheet">
+    <link href="/geii/css/Etudiant_CSS/menu.css" rel="stylesheet">
 
     </head>
 
--- view/vues_etudiant/etudiant.php
+++ view/vues_etudiant/etudiant.php
@@ -13,6 +13,10 @@
     <link href="/geii/bootstrap-5.3/css/bootstrap.min.css" rel="stylesheet">
     <script src="/geii/bootstrap-5.3/js/bootstrap.min.js"></script>
 
+    <!-- les bootstrap en ligne pour les dropdown -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     <!-- Autres JS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/geii/script/etudiant/boutonMenu.js"></script>
