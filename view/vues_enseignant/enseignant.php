1548ea7f708ee5b187cb71c09eb1f076ce115f7b
Marco Chm <91530804+MarcoCihm@users.noreply.github.com>
2023-06-19 21:10:45 +0200

    Update enseignant.php

--- view/vues_enseignant/enseignant.php
+++ view/vues_enseignant/enseignant.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+include '../../modele/enseignant/notes.php';
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,17 +12,24 @@
     <title>GEII - Espace Enseignant</title>
     <!-- https://getbootstrap.com/docs/5.3/getting-started/introduction/ -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap" rel="stylesheet">
-    <link href="../../bootstrap-5.3/css/bootstrap.min.css" rel="stylesheet">
-    <script src="../../bootstrap-5.3/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="../../css/Enseignant_CSS/enseignant.css">
     <link rel="stylesheet" href="../../css/Footer/style.css">
 </head>
 
+<?php
+include '../header.php';
+?>
+
 <body>
 
-    <?php
-    include '../../modele/enseignant/notes.php';
-    ?>
+
 
     <div class="container">
         <h1 class="text-center mt-5">👨‍🏫 GEII - Espace Enseignant 👩‍🏫</h1>
@@ -181,7 +192,7 @@
                                     <td>
                                         <?php if (isset($notes[$etudiant['id_etudiant']][$i])) { ?>
 
-    <!-- L'indentation est comme ceci car cela définit les espaces des informations affichées lors du survol de la souris -->
+                                            <!-- L'indentation est comme ceci car cela définit les espaces des informations affichées lors du survol de la souris -->
                                             <span title="Intitulé : <?php echo htmlspecialchars($notes[$etudiant['id_etudiant']][$i]['intitule']); ?> 
 Date : <?php echo htmlspecialchars($notes[$etudiant['id_etudiant']][$i]['date']); ?>
 
@@ -216,5 +227,6 @@ Commentaire : <?php echo htmlspecialchars($notes[$etudiant['id_etudiant']][$i]['
     </div>
 
 </body>
+
 </html>
 <?php include '../footer.php' ?>
