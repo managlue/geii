9effac0b51c14b60e0b856ef4e13876dd80af74e
managlue <125822488+managlue@users.noreply.github.com>
2023-06-19 18:42:52 +0200

    offres d'alternances pour les étudiants considérée fini

--- modele/etudiant/getAlternance.php
+++ modele/etudiant/getAlternance.php
@@ -12,6 +12,14 @@
     else {
 ?>
 
+<style>
+  .custom-card-image {
+    width: 250px;
+    height: 150px;
+    object-fit: cover;
+  }
+</style>
+
 <div class="container text-center">
     <div class="row">
 
@@ -23,15 +31,31 @@
         <div class="col-sm">
             <div class="card">
 
-<?php echo '<img src="' . $imagePath . $offre['image'] . '" class="card-img-top img-thumbnail">'; ?>
+<?php echo '<img src="' . $imagePath . $offre['image'] . '" class="custom-card-image mx-auto card-img-top img-thumbnail">'; ?>
 
                 <div class="card-body">
-                    <button type="button" class="btn btn-lg btn-danger" <?php echo 'data-bs-toggle="popover' . $cpt .'"'; ?> 
+                    <button type="button" class="btn btn-lg btn-danger"
 
+<?php echo 'data-bs-toggle="popover' . $cpt .'"'; ?> 
 <?php echo 'data-bs-content="' . $offre['description_entr'] . '"'; ?> >
 <?php echo '<h5 class="card-title">' . $offre['nom_entreprise'] . '</h5>'; ?>
 
-                    </button>
+                    </button><br><br>
+                    <button type="button" class="btn btn-lg btn-primary"
+
+<?php echo 'data-bs-toggle="popover' . $cpt .'a"'; ?>
+<?php echo 'data-bs-content="' . $offre['sujet_offre'] . '"'; ?> >
+<?php echo '<h5 class="card-title">' . $offre['titre_offre'] . '</h5>'; ?>
+
+                    </button><br><br>
+                    <p class="fs-5">
+
+<?php echo $offre['lieu'] . '<br>'; ?>
+<?php echo 'Rémunération : ' . $offre['remuneration'] . '<br>'; ?>
+<?php echo 'Date limite : ' . $offre['date_limite'] . '<br>'; ?>
+<?php echo 'Postuler : ' . $offre['postuler']; ?> 
+
+                    </p>
                 </div>
             </div>
         </div>
@@ -40,12 +64,13 @@
         var popover = new bootstrap.Popover(document.querySelector(
             <?php echo "'[data-bs-toggle=" . '"popover' . $cpt . '"]' . "'"; ?>
         ));
-
-        // dismissable
-        const popover = new bootstrap.Popover('.popover-dismiss', {
-            trigger: 'focus'
-        })
+        var popovera = new bootstrap.Popover(document.querySelector(
+            <?php echo "'[data-bs-toggle=" . '"popover' . $cpt . 'a"]' . "'"; ?>
+        ));
     </script>
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
+
 
 <?php
     }
@@ -57,3 +82,8 @@
 <?php
     }
 ?>
+
+<!-- 
+    vois d'améliorations :
+    mettre des popovers "Dismiss on next click"
+ -->
